ff009191443ba989fe7a5fb69a4cadb83717b98a
berce <75228263+must4f@users.noreply.github.com>
2021-06-17 00:08:40 +0300

    Create pip_install.py

---
+++ userbot/utils/pip_install.py
@@ -0,0 +1,14 @@
+# SiriUserBot - Pip Install
+
+#Credit: sandy1709
+
+
+from subprocess import PIPE, Popen
+
+
+def install_pip(pipfile):
+    print(f"installing {pipfile}")
+    pip_cmd = ["pip", "install", f"{pipfile}"]
+    process = Popen(pip_cmd, stdout=PIPE, stderr=PIPE)
+    stdout, stderr = process.communicate()
+    return stdout
