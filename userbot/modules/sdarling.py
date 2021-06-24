c6fb1254bc93886960f0c170f3d076c698da1cb4
berce <75228263+must4f@users.noreply.github.com>
2021-06-24 22:19:44 +0300

    fixes

--- userbot/modules/sdarling.py
+++ userbot/modules/sdarling.py
@@ -1,24 +1,24 @@
 from .herokuhelper import heroku_var, app
 from userbot.cmdhelp import CmdHelp
 from userbot.events import register
-from userbot import SEVGILIM
+from userbot import SEVGILI
 import heroku3
 
 @register(pattern='^.adddarling$', replyneeded=True)
 async def adddarling(e):
-    global SEVGILIM
+    global SEVGILI
     reply = await e.get_reply_message()
     reply_user = await e.client.get_entity(reply.from_id)
-    SEVGILIM = reply_user.id
-    await e.edit('`Sevgiliniz Eklendi❤️`')
+    SEVGILI = int(reply_user.id)
+    await e.edit('`Sevgiliniz eklendi.. ❤️\nGlobal değerlere işlenmesi için lütfen 2 dakika bekleyin!`')
     if heroku_var and app:
         heroku_var["SEVGILI"] = reply_user.id
     return True
 
 @register(pattern='^.rmdarling$')
 async def adddarling(e):
-    global SEVGILIM
-    SEVGILIM = None
+    global SEVGILI
+    SEVGILI = None
     await e.edit('`Sevgiliniz Silindi💔`')
     if heroku_var and app:
         del heroku_var["SEVGILI"]
