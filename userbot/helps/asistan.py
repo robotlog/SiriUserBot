7b4c5519b3a986932f42d8c9635d25eaf5da50ba
berce <75228263+must4f@users.noreply.github.com>
2021-06-24 19:30:59 +0300

    Create asistan.py

---
+++ userbot/helps/asistan.py
@@ -0,0 +1,12 @@
+from asistan import MYID
+
+async def bana_mi_diyo(ups):
+    if ups.is_reply:
+        reply = await ups.get_reply_message()
+        reply_user = await ups.client.get_entity(reply.from_id)
+        ren = reply_user.id
+        if ren == MYID:
+            return True
+        return False
+    return False
+
