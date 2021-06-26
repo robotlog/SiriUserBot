fdf49ce7c62abebb84fd4edd32d0ec3aab2e5266
berce <75228263+must4f@users.noreply.github.com>
2021-06-26 18:56:36 +0300

    Update asistan.py

--- userbot/helps/asistan.py
+++ userbot/helps/asistan.py
@@ -1,4 +1,18 @@
-from userbot import MYID
+from userbot import MYID, SEVGILI, SUDO_ID
+
+async def edit_or_reply(event, text):
+    try:
+        THISSUDO = SUDO_ID or [0]
+        THISSEW = SEVGILI or [0]
+        if event.from_id in THISSUDO or event.from_id in THISSEW:
+            reply_to = await event.get_reply_message()
+            if reply_to:
+                return await reply_to.reply(text)
+            return await event.reply(text)
+        return await event.edit(text)
+    except:
+        return await event.edit(text)
+
 
 async def bana_mi_diyo(ups):
     if ups.is_reply:
