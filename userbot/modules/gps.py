1f12bac1612830370932b226a95dc2c040a6fcda
berce <75228263+must4f@users.noreply.github.com>
2021-06-28 17:49:34 +0300

    Update gps.py

--- userbot/modules/gps.py
+++ userbot/modules/gps.py
@@ -4,10 +4,8 @@ from telethon.tl import types
 from userbot.events import register
 from userbot.cmdhelp import CmdHelp
 
-@register(outgoing=True, pattern=r"^.gps (.*)")
+@register(pattern=r"^.gps (.*)")
 async def gps(event):
-    if event.fwd_from:
-        return
     reply_to_id = event.message.id
     if event.reply_to_msg_id:
         reply_to_id = event.reply_to_msg_id
@@ -18,7 +16,7 @@ async def gps(event):
 
     await event.edit("**Buluyorum...**")
 
-    geolocator = Nominatim(user_agent="erdembey1")
+    geolocator = Nominatim(user_agent="iOS")
     geoloc = geolocator.geocode(input_str)
     if geoloc:
         lon = geoloc.longitude
