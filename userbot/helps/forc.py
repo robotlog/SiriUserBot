7615e7a32f5f24b9072a9c82dfcf6cf79c67b9eb
berce <75228263+must4f@users.noreply.github.com>
2021-07-15 16:56:00 +0300

    Update forc.py

--- userbot/helps/forc.py
+++ userbot/helps/forc.py
@@ -2,18 +2,27 @@ from telethon.errors.rpcerrorlist import YouBlockedUserError
 from telethon.tl.functions.contacts import UnblockRequest
 from userbot import bot
 
-async def force_send_message(event, text, chat=None):
+async def force_send_message(event, text, forward=False, chat=None):
     cHat = chat if chat else event.chat_id
-    try:
-        e = await event.client.send_message(
-            cHat,
-            text
-        )
-    except YouBlockedUserError:
-        await event.client(UnblockRequest(cHat)) 
-        e = await event.client.send_message(
-            cHat,
-            text
-        )
+    if not forward:
+        try:
+            e = await event.client.send_message(
+                cHat,
+                text
+            )
+        except YouBlockedUserError:
+            await event.client(UnblockRequest(cHat)) 
+            e = await event.client.send_message(
+                cHat,
+                text
+            )
+    else:
+        try:
+            e = await event.client.forward_messages(cHat, text)
+            e = await e
+        except YouBlockedUserError:
+            await event.client(UnblockRequest(cHat)) 
+            e = await event.client.forward_messages(cHat, text)
+            e = await e
     return e
 
