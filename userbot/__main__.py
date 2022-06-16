b057c3cd87cbc5606d8004b97e888a29682abab3
berce <75228263+must4f@users.noreply.github.com>
2022-06-16 16:44:05 +0300

    requirements download

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -1,1 +1,14 @@
+from utils.pip_install import install_pip
+import os
+
+os.chdir(os.pardir)
+liste = list()
+liste.append(os.getcwd())
+liste.append("requirements.txt")
+dizin = os.sep.join(liste)
+
+with open(dizin,"r") as dosya:
+   for satir in dosya:
+        install_pip(satir.rstrip("/n"))
+        
 from main import *
